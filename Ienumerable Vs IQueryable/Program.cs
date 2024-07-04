6538467fe9d7118e4cd26bd1155201615d76542c
nimahaji <nimahaji1395@gmail.com>
2024-07-04 02:50:50 -0700

    we make list and ienumerable and iquryable from book

--- Ienumerable Vs IQueryable/Program.cs
+++ Ienumerable Vs IQueryable/Program.cs
@@ -10,8 +10,25 @@ namespace Ienumerable_Vs_IQueryable
     {
         static void Main(string[] args)
         {
-            Library_DBEntities Books = new Library_DBEntities();
-            Books
+            //normal case to read books from DB
+            Library_DBEntities DB = new Library_DBEntities();
+            var list = DB.Libraries.ToList();
+            foreach (var book in list)
+            {
+                Console.WriteLine($"Book name is : {book.BookName} Color is : {book.BookColor} Writer is : {book.BookWriter}");
+            }
+
+            //IQuryable Case : 
+            IQueryable<Library> list1 = DB.Libraries.Where(n => n.BookColor == "Blue" && n.BookName== "The Housemaid Is Watching");
+            //in sql :
+            //SELECT * FROM Books AS [n] WHERE [n].[BookColor] == 'Blue' AND [n].[BookName] == 'The Housemaid Is Watching'
+
+            //IEnumerable Case :
+            IEnumerable<Library> Book = DB.Libraries.AsEnumerable().Where(b=>b.BookColor == "Blue" && b.BookName == 'The Housemaid Is Watching');
+            //in sql :
+            //SELECT* FROM Books AS[n]
+
+            Console.ReadKey();
         }
     }
 }
